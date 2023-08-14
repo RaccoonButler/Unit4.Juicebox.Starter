f7ea9755eb0776860765e33ec92745ea964f6ca3
RaccoonButler <raccoonutau@gmail.com>
2023-08-14 14:10:02 -0500

    attempt at requireUser logic

--- api/utils.js
+++ api/utils.js
@@ -1,9 +1,9 @@
 function requireUser(req, res, next) {
-  // Check if user is logged in (you need to implement this check based on your application's authentication logic)
-  if (/* user is logged in */) {
-    // Attach user information to the request object
-    req.user = /* user object or user data */;
-    next(); // Continue to the next middleware or route handler
+  // Check if user is logged in based on req.user
+  if (req.user) {
+    // User is logged in, attach user information to the request object
+    req.user = req.user;
+    next();
   } else {
     // User is not logged in, send unauthorized response
     res.status(401).json({ error: 'Unauthorized' });
@@ -13,3 +13,4 @@ function requireUser(req, res, next) {
 module.exports = {
   requireUser
 };
+

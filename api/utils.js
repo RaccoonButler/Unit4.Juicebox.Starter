a0a3c139eebfc59a3f0fccee9b2481fa27d4b9c7
RaccoonButler <raccoonutau@gmail.com>
2023-08-14 14:02:12 -0500

    started on requireUser

--- api/utils.js
+++ api/utils.js
@@ -1,7 +1,15 @@
 function requireUser(req, res, next) {
-
+  // Check if user is logged in (you need to implement this check based on your application's authentication logic)
+  if (/* user is logged in */) {
+    // Attach user information to the request object
+    req.user = /* user object or user data */;
+    next(); // Continue to the next middleware or route handler
+  } else {
+    // User is not logged in, send unauthorized response
+    res.status(401).json({ error: 'Unauthorized' });
+  }
 }
 
 module.exports = {
   requireUser
-}
+};
